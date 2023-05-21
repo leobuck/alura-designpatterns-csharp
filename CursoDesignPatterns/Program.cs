dca5c565faabd4d9148f447787a18d50e25ab679
leobuck <leobuck12@gmail.com>
2023-05-21 19:56:17 -0300

    Estratégia para o imposto ICCC

--- CursoDesignPatterns/Program.cs
+++ CursoDesignPatterns/Program.cs
@@ -2,6 +2,7 @@
 
 Imposto iss = new ISS();
 Imposto icms = new ICMS();
+Imposto iccc = new ICCC();
 
 Orcamento orcamento = new Orcamento(500);
 
@@ -9,5 +10,6 @@ CalculadorDeImpostos calculador = new CalculadorDeImpostos();
 
 calculador.RealizaCalculo(orcamento, icms);
 calculador.RealizaCalculo(orcamento, iss);
+calculador.RealizaCalculo(orcamento, iccc);
 
 Console.ReadKey();
